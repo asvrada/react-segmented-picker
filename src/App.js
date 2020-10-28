6480940f5c36b0bb9aba0001f6602344d177e5d0
Zijie Wu <noojeff@gmail.com>
2020-10-27 18:10:21 -0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
+import SegmentedPicker from './components/SegmentedPicker';
+
+// Demo page
 function App() {
+  const [selection, setSelection] = useState(0);
+  const options = ['Apple', 'Orange', 'Pear', 'Watermelon'];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>You selected</p>
+      <p>#{selection}: {options[selection]}</p>
+
+      <SegmentedPicker
+        options={options}
+        selection={selection}
+        onSelectionChange={(newSelection) => {
+          setSelection(newSelection);
+        }}
+      />
     </div>
   );
 }
---
+++ src/components/SegmentedPicker.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import '../styles/segmented-picker.scss';
+
+const SPACE = ' ';
+
+const DivSegmentedPicker = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 2px;
+  border-radius: 7px;
+  background-color: rgb(238, 238, 239);
+  overflow: hidden;
+`;
+
+const DivSeparator = styled.div`
+  display: inline-block;
+  opacity: ${props => props.opacity};
+  transition: opacity 0.2s ease-in-out;
+  background-color: rgb(212, 212, 215);
+  width: 1px;
+  height: 1em;
+`;
+
+const DivOption = styled.div`
+  display: inline-block;
+  text-align: center;
+  flex-grow: 1;
+  padding: 2px 1em;
+  border-radius: 5px;
+  cursor: pointer;
+  user-select: none;
+`;
+
+// options is a list of string
+const SegmentedPicker = ({ options, selection, onSelectionChange, className }) => {
+
+  const componentOptions = options.map((each, index) => {
+    // don't show if current or previous index is selected
+    const showSeparator = !(index === selection || (index - 1) === selection);
+
+    const opacity = showSeparator ? 1 : 0;
+
+    return (
+      <React.Fragment key={index}>
+        {/* Hide the separator for item at index 0 */}
+        {index !== 0 && <DivSeparator opacity={opacity}>{SPACE}</DivSeparator>}
+
+        <DivOption className={((index === selection) ? 'react-segmented-picker-selected-option' : '')}
+             onClick={() => {
+               onSelectionChange(index);
+             }}
+        >
+          {each}
+        </DivOption>
+      </React.Fragment>
+    );
+  });
+
+  return (
+    <DivSegmentedPicker className={className}>
+      {componentOptions}
+    </DivSegmentedPicker>
+  );
+};
+
+export default SegmentedPicker;
