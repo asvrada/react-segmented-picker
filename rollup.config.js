3e5121ca1c0ba6d633162ceb93ae7c9490a4866a
Zijie Wu <noojeff@gmail.com>
2021-08-15 15:02:57 -0700

    Remove resolve and commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,4 @@
 import buble from '@rollup/plugin-buble';
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
 import filesize from 'rollup-plugin-filesize';
 import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
@@ -8,10 +6,10 @@ import typescript from 'rollup-plugin-typescript2';
 
 import pkg from './package.json';
 
-// ESM build
+// only generate ES module build
 export default {
   input: './src/install.js',
-  external: ['react', 'react-dom', 'styled-components'],
+  external: ['react', 'react/jsx-runtime', 'react-dom', 'styled-components'],
   output: [
     { file: pkg.module, format: 'es' },
   ],
@@ -23,8 +21,6 @@ export default {
         dangerousTaggedTemplateString: true,
       },
     }),
-    resolve(),
-    commonjs(),
     postcss({
       extract: false,
       minimize: true,
